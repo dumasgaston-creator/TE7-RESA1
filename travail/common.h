65299e2cabc9f14f768d427d704b8ee06cd01e1f
Gaston Dumas <gdumas004@enseirb-matmeca.fr>
2026-09-22 17:27:51 +0200

    Modif server req1.4

--- travail/common.h
+++ travail/common.h
@@ -1,6 +1,7 @@
 #define MSG_LEN 1024
 #define SERV_PORT "8080"
 #define SERV_ADDR "127.0.0.1"
+#define MAX_CLIENTS 100
 
 struct info{
     short s;
--- travail/server.c
+++ travail/server.c
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,70 +8,39 @@
 
 #include "common.h"
 
-void echo_server(int sockfd) {
-	char buff[MSG_LEN];
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
+void die(int ret, char* msg){
+	if(ret<0){
+		perror(msg);
+		exit(EXIT_FAILURE);
 	}
+
 }
 
-int handle_bind() {
-	struct addrinfo hints, *result, *rp;
-	int sfd;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
-		perror("getaddrinfo()");
-		exit(EXIT_FAILURE);
-	}
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-		rp->ai_protocol);
-		if (sfd == -1) {
-			continue;
-		}
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
-			break;
-		}
-		close(sfd);
-	}
-	if (rp == NULL) {
-		fprintf(stderr, "Could not bind\n");
-		exit(EXIT_FAILURE);
-	}
-	freeaddrinfo(result);
-	return sfd;
-}
+int main(int argc, char *argv[]) {
 
-int main() {
-	struct sockaddr cli;
-	int sfd, connfd;
-	socklen_t len;
-	sfd = handle_bind();
-	if ((listen(sfd, SOMAXCONN)) != 0) {
-		perror("listen()\n");
-		exit(EXIT_FAILURE);
+	if (argc =! 2){
+		die(argc, " Mets l'argument\n");
 	}
-	len = sizeof(cli);
-	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
-		perror("accept()\n");
-		exit(EXIT_FAILURE);
-	}
-	echo_server(connfd);
-	close(sfd);
-	return EXIT_SUCCESS;
-}
+	int port = atoi(argv[1]);
 
+	int listen_fd = socket(AF_INET,SOCK_STREAM, 0);
+    die(listen_fd, "On creating");
+
+    
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;// s.addr stocke l'@ ip sous forme de bit
+
+    int ret_value;
+    ret_value = bind( listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    die(ret_value, "on binding");
+
+    ret_value = listen(listen_fd, BACKLOG);
+    die(ret_value, "on listening");  
+    
+    struct sockaddr_in client_addr;    
+    socklen_t adress_len = sizeof(struct sockaddr_in);  
+    return EXIT_SUCCESS;
+}
